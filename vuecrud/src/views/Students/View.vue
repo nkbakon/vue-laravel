72485cc513395b5552150dfa8c32934c645cb7e0
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2023-09-30 23:37:37 +0530

    Inital

---
+++ vuecrud/src/views/Students/View.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    Students
+                    <RouterLink to="/student/create" class="btn btn-primary float-end">
+                        Add Student
+                    </RouterLink>
+                </h4>
+            </div>
+        </div>
+    </div>
+</template>
+  
